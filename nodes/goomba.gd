fd5d7993cd4cb8e693141baf0628c1f7afc5a4c1
LilyJones22 <afanofzelda@gmail.com>
2025-11-06 03:12:21 -0500

    add goomba replacement

--- nodes/goomba.gd
+++ nodes/goomba.gd
@@ -2,11 +2,49 @@ extends Area2D
 
 var health = 1
 
+@export var speed: float = 40.0
+@export var patrol_distance: float = 200.0
+
+@onready var sprite:= $Sprite2D
+@onready var wall_check:= $WallCheck
+@onready var ground_check:= $GroundCheck
+
+var direction: int = -1
+var distance_traveled: float = 0.0
+var start_x: float
+
+func _ready():
+	start_x = global_position.x
+	if wall_check:
+		wall_check.enabled = true
+	if ground_check:
+		ground_check.enabled = true
+		
+func turn_around():
+	direction *= -1
+	start_x = global_position.x
+	distance_traveled = 0.0
+	
+	if wall_check:
+		wall_check.position.x = abs(wall_check.position.x) * direction
+	if ground_check:
+		ground_check.position.x = abs(ground_check.position.x) * direction
 
 			
 func _physics_process(delta):
 	if health == 0:
 		queue_free()
+		
+	position.x += direction * speed * delta
+	distance_traveled = abs(global_position.x - start_x)
+	if distance_traveled >= patrol_distance:
+		turn_around()
+		
+	if wall_check and wall_check.is_colliding():
+		turn_around()
+		
+	if ground_check and not ground_check.is_colliding():
+		turn_around()
 			
 
 func _on_area_entered(area):
